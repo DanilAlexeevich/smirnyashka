a35af8758c9ee3535b0fbcaa2f22cbef51cd1ee1
AlexeyD26 <derga4iovv.alex@gmail.com>
2024-08-13 17:24:59 +0300

    new

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import style from './style.module.scss'
 
 function App() {
  
 
   return (
     <>
-      <img src="./vas.jpg" alt="" className='vas'/>
+      <header className={style.header}>
+        <img src="./vas.jpg" alt="vasiliev" className={style.vas}/>
+        <h1>BookChange</h1>
+      </header>
     </>
   )
 }
